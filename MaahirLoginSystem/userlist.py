5999b8038bb43045d73b926f65e8b30df8164882
Maahir Sachdev <51933563+maahir30@users.noreply.github.com>
2020-06-03 18:29:23 -0700

    removed some users

--- MaahirLoginSystem/userlist.py
+++ MaahirLoginSystem/userlist.py
@@ -1,1 +1,2 @@
-users = {"MaahirSachdev": "password123", "Hello123": "password", "Maahir30": "123456", "maahi": "m30"}
+#Defualt users list, once you create an account, the your username should show up here.
+users = {"MaahirSachdev": "password123", "Hello123": "password", "Maahir30": "123456"}
